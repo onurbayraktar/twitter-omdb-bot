e2a91b8603d0c510903509d7d7b1fb32c7ab0f49
onurbayraktar <32903278+onurbayraktar@users.noreply.github.com>
2019-10-16 15:51:22 +0300

    delete config

--- config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports =  {
-    consumer_key:         '7E8CcY17D3Rnq78rUIceMhGKB',
-    consumer_secret:      '3BJrluddbfoxJbkSkGwW0OizGLwPDkTcLQLXbwd9X3kAUNwXPm',
-    access_token:         '711990896800038913-Zr6EQruc4NxZ8hd6lVSTOgSExzJGR3A',
-    access_token_secret:  'iQ6cP5Oj7v15thAOzEaGxKRpSiFAfJ1bDHBFZ7dm4CQdS',
-}
